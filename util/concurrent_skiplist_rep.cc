d5a2e491648b6dc9d7781546f38b80abfef01103
Igor Zablotchi <igor.zablotchi@epfl.ch>
2014-04-12 16:38:56 +0200

    implemented dummy iterator

--- util/concurrent_skiplist_rep.cc
+++ util/concurrent_skiplist_rep.cc
@@ -43,12 +43,12 @@ public:
   virtual void Get(const LookupKey& k, void* callback_args,
                    bool (*callback_func)(void* arg,
                                          const char* entry)) override {
-    ConcurrentSkipListRep::Iterator iter(&skip_list_);
-    Slice dummy_slice;
-    for (iter.Seek(dummy_slice, k.memtable_key().data());
-         iter.Valid() && callback_func(callback_args, iter.key());
-         iter.Next()) {
-    }
+    // ConcurrentSkipListRep::Iterator iter(&skip_list_);
+    // Slice dummy_slice;
+    // for (iter.Seek(dummy_slice, k.memtable_key().data());
+    //      iter.Valid() && callback_func(callback_args, iter.key());
+    //      iter.Next()) {
+    // }
   }
 
   virtual ~ConcurrentSkipListRep() override { }
@@ -57,69 +57,71 @@ public:
 
   // ADD HERE - STUPID ITERATOR
   class Iterator : public MemTableRep::Iterator {
-    SkipList<const char*, const MemTableRep::KeyComparator&>::Iterator iter_;
+    //SkipList<const char*, const MemTableRep::KeyComparator&>::Iterator iter_;
    public:
     // Initialize an iterator over the specified list.
     // The returned iterator is not valid.
-    explicit Iterator(
-      const SkipList<const char*, const MemTableRep::KeyComparator&>* list
-    ) : iter_(list) { }
+    explicit Iterator() {}
 
     virtual ~Iterator() override { }
 
     // Returns true iff the iterator is positioned at a valid node.
     virtual bool Valid() const override {
-      return iter_.Valid();
+      //return iter_.Valid();
+      return false;
     }
 
     // Returns the key at the current position.
     // REQUIRES: Valid()
     virtual const char* key() const override {
-      return iter_.key();
+      //return iter_.key();
+      return nullptr;
     }
 
     // Advances to the next position.
     // REQUIRES: Valid()
     virtual void Next() override {
-      iter_.Next();
+      //iter_.Next();
     }
 
     // Advances to the previous position.
     // REQUIRES: Valid()
     virtual void Prev() override {
-      iter_.Prev();
+      //iter_.Prev();
     }
 
     // Advance to the first entry with a key >= target
     virtual void Seek(const Slice& user_key, const char* memtable_key)
         override {
-      if (memtable_key != nullptr) {
-        iter_.Seek(memtable_key);
-      } else {
-        iter_.Seek(EncodeKey(&tmp_, user_key));
-      }
+      // if (memtable_key != nullptr) {
+      //   iter_.Seek(memtable_key);
+      // } else {
+      //   iter_.Seek(EncodeKey(&tmp_, user_key));
+      // }
     }
 
     // Position at the first entry in list.
     // Final state of iterator is Valid() iff list is not empty.
     virtual void SeekToFirst() override {
-      iter_.SeekToFirst();
+      // iter_.SeekToFirst();
     }
 
     // Position at the last entry in list.
     // Final state of iterator is Valid() iff list is not empty.
     virtual void SeekToLast() override {
-      iter_.SeekToLast();
+      // iter_.SeekToLast();
     }
-   protected:
-    std::string tmp_;       // For passing to EncodeKey
+   //protected:
+    //std::string tmp_;       // For passing to EncodeKey
   };
 
   // Unhide default implementations of GetIterator
   using MemTableRep::GetIterator;
 
   virtual MemTableRep::Iterator* GetIterator() override {
-    return new ConcurrentSkipListRep::Iterator(&skip_list_);
+    // return new ConcurrentSkipListRep::Iterator(&skip_list_);
+    return nullptr;
+
   }
 };
 }
