c28f83c7e724ceec247eb910eb4d21b09623aba9
balmau <oana.balmau@epfl.ch>
2014-05-23 05:38:20 -0700

    small modification to test script

--- test_configs.sh
+++ test_configs.sh
@@ -4,9 +4,9 @@ now=`date`
 filename=TestRun_$now.txt
 echo "Test results \n" > $filename
 
-for numThreads in 1 2 # 4 8 16 20
+for update in 0 10 50 100
 do
-	for update in 0 #10 50 100
+	for numThreads in 1 2 4 8 16 20
 	do
 		./db_test -u$update -d1 -n$numThreads -i1024 | grep -w 'stopping\|Options' >> $filename
 	done
