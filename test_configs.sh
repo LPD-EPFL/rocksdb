24555f98fa2e5b17c3ee6b23073d9f70e8b58bc4
Oana Balmau <oana.balmau@epfl.ch>
2014-06-03 21:03:38 +0200

    added testing and parsing scripts to the testing branch

--- test_configs.sh
+++ test_configs.sh
@@ -6,8 +6,11 @@ echo "Test results \n" > $filename
 
 for update in 0 10 50 100
 do
-	for numThreads in 1 2 4 8 16 20
+	for numThreads in 1 2 4 8 16 19
 	do
 		./db_test -u$update -d1 -n$numThreads -i1024 | grep -w 'stopping\|Options' >> $filename
 	done
 done
+
+perl parse_results.pl "$filename"
+echo "Done!\n"
