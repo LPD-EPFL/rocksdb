04b8c1c05bce5679e049bf1ff9c9b19c633b7b56
Igor Zablotchi <zablotch@lpdxeon2680.epfl.ch>
2014-06-03 21:27:05 +0200

    added more options to the test script

--- test_configs.sh
+++ test_configs.sh
@@ -8,7 +8,8 @@ for update in 0 10 50 100
 do
 	for numThreads in 1 2 4 8 16 19
 	do
-		./db_test -u$update -d1 -n$numThreads -i1024 | grep -w 'stopping\|Options' >> $filename
+		#./db_test -u$update -d10 -n$numThreads -i1048576 | grep -w 'stopping\|Options' >> $filename
+		./db_test -u$update -d10 -n$numThreads -i1024 | grep -w 'stopping\|Options' >> $filename
 	done
 done
 
